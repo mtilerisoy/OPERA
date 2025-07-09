df8bc1507d8f7ede40ab7a82eac07375d2fa689f
Mustafa Talha Ilerisoy <mtilerisoy@gmail.com>
2025-07-09 16:08:59 +0200

    implement snellius script to run test job

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=test_opereCE
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --partition=gpu_a100
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=00:30:00
+#SBATCH --output=test_%j.out
+set -x # Enable script debugging
+ 
+echo "Loading module 2023..."
+module load 2023
+ 
+echo "Loading CUDA/12.1.1..."
+module load CUDA/12.1.1
+module load cudnn/8.6 # Ensure cuDNN module is loaded if separate
+ 
+echo "Updating LD_LIBRARY_PATH..."
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$HOME/miniconda3/lib:$LD_LIBRARY_PATH
+ 
+echo "Activating virtual environment..."
+source /home/milerisoy/miniconda3/bin/activate audio
+ 
+echo "Verifying Python and CUDA..."
+which python
+python --version
+python -c "import torch; print('CUDA Available:', torch.cuda.is_available()); print('CUDA Devices:', torch.cuda.device_count())"
+ 
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+ 
+echo "Starting training script..."
+# python aug/scripts/generate_test_set.py
+sh scripts/eval_icbhi.sh
