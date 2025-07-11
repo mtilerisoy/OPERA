a0b316f6c3803a6d7fe8508c39fcedf010b5f827
Mustafa Talha Ilerisoy <mtilerisoy@gmail.com>
2025-07-11 13:31:02 +0200

    evaluate all the models for icbhi

--- scripts/eval_icbhi.sh
+++ scripts/eval_icbhi.sh
@@ -1,7 +1,21 @@
-######## EVAL ICBHI OPERA ########
-echo extracting feature from $pretrain_model for downstream Task7;
-python -u src/benchmark/processing/icbhi_processing.py  --pretrain operaCE --dim 1280
+model_name="operaCT"
+dim=768
+python src/benchmark/processing/icbhi_processing.py --pretrain $model_name --dim $dim
+python src/benchmark/linear_eval.py --task icbhidisease --pretrain $model_name --dim $dim
 
-echo linear evaluation of $pretrain_model on downstream Task7;
-python src/benchmark/linear_eval.py --task icbhidisease  --pretrain operaCE --dim 1280
-################################
+model_name="operaCE"
+dim=1280
+python src/benchmark/processing/icbhi_processing.py --pretrain $model_name --dim $dim
+python src/benchmark/linear_eval.py --task icbhidisease --pretrain $model_name --dim $dim
+
+model_name="operaGT"
+dim=384
+python src/benchmark/processing/icbhi_processing.py --pretrain $model_name --dim $dim
+python src/benchmark/linear_eval.py --task icbhidisease --pretrain $model_name --dim $dim
+
+list="opensmile vggish clap audiomae"
+for i in $list;
+do
+python src/benchmark/processing/icbhi_processing.py --pretrain $i 
+python src/benchmark/linear_eval.py --task icbhidisease --pretrain $i
+done
